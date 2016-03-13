360c8bced5b0635ed1e0d832e03e69c1186b10b2
antojodickson <antojodickson@gmail.com>
2016-03-14 00:35:07 +0530

    addobs

--- client/app/addjob/addjob.controller.js
+++ client/app/addjob/addjob.controller.js
@@ -1,6 +1,23 @@
 'use strict';
 
 angular.module('jobportalApp')
-  .controller('AddjobCtrl', function ($scope) {
+  .controller('AddjobCtrl', function ($scope,$state,$http) {
     $scope.message = 'Hello';
+    $scope.company = {
+      name:'jobs test',
+      address:{},
+      contact:{}
+  };
+
+  $scope.submit = function() {
+    console.log("values", $scope.jobs);
+    $http.post('/api/jobs', $scope.jobs).then(function(company) {
+      console.log("jobs", jobs);
+      if(company) {
+        $state.go('jobs');
+      }
+    }, function(err) {
+      console.log("err", err);
+    });
+  }
   });
